5fdf02b4bc90ff7698f40c1af7545b2a9b5ada58
martaseisdedos <100596598+martaseisdedos@users.noreply.github.com>
2022-06-22 13:51:00 +0200

    Update ListaPeonBlanco.h

--- src/ListaPeonBlanco.h
+++ src/ListaPeonBlanco.h
@@ -10,10 +10,10 @@ private:
 public:
 	ListaPeonBlanco();
 
-	void mueve(float posx, float posy, float posxfinf, float posyfinf);
+	void mueve(Vector, Vector);
 	bool agregar(peon* p);
 	void dibuja();
-	//bool comparax(float posx);
+
 	void cambiacome(int indice);
 	void cambiavanza(int indice);
 
@@ -21,13 +21,14 @@ public:
 	float getx(int indice);
 	float gety(int indice);
 
-	//void resetcomeavanza();
 
-	int compara(float px, float py);
+
+	int compara(Vector);
 	bool comprobacioncome(int indice, float posxfinf, float posyfinf);
 	void setpos(int indice, float posx, float posy);
 
 	void setturno(int t);
 	int getturno();
+
 
 };
