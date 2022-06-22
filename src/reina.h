206c6cfa6cccac388717c01c1c2c04d14c9074c1
martaseisdedos <100596598+martaseisdedos@users.noreply.github.com>
2022-06-22 11:56:16 +0200

    Update reina.h

--- src/reina.h
+++ src/reina.h
@@ -1,6 +1,4 @@
 #pragma once
-#define MOVIMIENTO_CORRECTO 1
-#define ERROR 0
 
 #include "Pieza.h"
 
@@ -9,16 +7,13 @@ class reina:public Pieza
 private:
 	
 public:
-	reina();
-	reina(float x, float y, unsigned char r, unsigned char a, unsigned char v);
+	reina():Pieza(){}
+	reina(Vector preina, int c);
 
 	void dibuja();
 
-	int mov_correcto(float posxfin, float posyfin);
-	void movimientos(float posxfin, float posyfin);
-	void mueve(float posxinif, float posyinif, float posxfinf, float posyfinf);
+	int mov_correcto(Vector);
+	void movimientos(Vector);
 
-	int getturno();
-	void setturno(int t);
 };
 
