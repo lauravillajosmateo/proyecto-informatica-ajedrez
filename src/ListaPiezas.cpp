c7e381df95a7c6bdc3471ab1a94c98085e7766f9
evagombar <99722831+evagombar@users.noreply.github.com>
2022-06-30 00:06:33 +0200

    Update ListaPiezas.cpp

--- src/ListaPiezas.cpp
+++ src/ListaPiezas.cpp
@@ -82,6 +82,7 @@ void ListaPiezas::cambiarturnos()
 {
 	for (int i = 0; i < 32; i++)
 	{
+		dibujatblancas();
 		if (lista[i]->getturno() == 1 && lista[i]->getcolor() == 0)//si una blanca se ha movido
 		{
 			for (int j = 0; j < 32; j++)
@@ -100,6 +101,7 @@ void ListaPiezas::cambiarturnos()
 
 		else if (lista[i]->getturno() == 1 && lista[i]->getcolor() == 1)
 		{
+			dibujatnegras();
 			for (int j = 0; j < 32; j++)
 			{
 				if (lista[j]->getcolor() == 1)
@@ -136,6 +138,80 @@ void ListaPiezas::comer(Pieza* p, Vector v) //vector de posicion final
 		}
 	}
 
+}
+
+void ListaPiezas::dibujatblancas()
+{
+	gluLookAt(0, 7.5, 30,
+	0.0, 7.5, 0.0,
+	0.0, 1.0, 0.0);
+	ETSIDI::setTextColor(255, 255, 255);
+	ETSIDI::setFont("fuentes/Bitwise.ttf", 20);
+	ETSIDI::printxy("TURNO DE ANGELES", -8.5, 25);
+	ETSIDI::setTextColor(255, 255, 255);
+	ETSIDI::setFont("fuentes/Bitwise.ttf", 20);
+	ETSIDI::printxy(" 1 ", -10.5, 0);
+	ETSIDI::setTextColor(255, 255, 255);
+	ETSIDI::setFont("fuentes/Bitwise.ttf", 20);
+	ETSIDI::printxy(" 2 ", -10.5, 3.5);
+	ETSIDI::setTextColor(255, 255, 255);
+	ETSIDI::setFont("fuentes/Bitwise.ttf", 20);
+	ETSIDI::printxy(" 1   2   3   4   5   6    7   8", -8, -2);
+	ETSIDI::setTextColor(255, 255, 255);
+	ETSIDI::setFont("fuentes/Bitwise.ttf", 20);
+	ETSIDI::printxy(" 3 ", -10.5, 6.5);
+	ETSIDI::setTextColor(255, 255, 255);
+	ETSIDI::setFont("fuentes/Bitwise.ttf", 20);
+	ETSIDI::printxy(" 4  ", -10.5, 9.5);
+	ETSIDI::setTextColor(255, 255, 255);
+	ETSIDI::setFont("fuentes/Bitwise.ttf", 20);
+	ETSIDI::printxy(" 5 ", -10.5, 12.5);
+	ETSIDI::setTextColor(255, 255, 255);
+	ETSIDI::setFont("fuentes/Bitwise.ttf", 20);
+	ETSIDI::printxy(" 6  ", -10.5, 15.5);
+	ETSIDI::setTextColor(255, 255, 255);
+	ETSIDI::setFont("fuentes/Bitwise.ttf", 20);
+	ETSIDI::printxy(" 7 ", -10.5, 18.5);
+	ETSIDI::setTextColor(255, 255, 255);
+	ETSIDI::setFont("fuentes/Bitwise.ttf", 20);
+	ETSIDI::printxy(" 8", -10.5, 21.5);
+}
+
+void ListaPiezas::dibujatnegras()
+{
+	gluLookAt(0, 7.5, 30,
+	0.0, 7.5, 0.0,
+	0.0, 1.0, 0.0);
+	ETSIDI::setTextColor(255, 0, 0);
+	ETSIDI::setFont("fuentes/Bitwise.ttf", 20);
+	ETSIDI::printxy("TURNO DE DEMONIOS", -8.5, 25);
+	ETSIDI::setTextColor(255, 255, 255);
+	ETSIDI::setFont("fuentes/Bitwise.ttf", 20);
+	ETSIDI::printxy(" 1 ", -10.5, 0);
+	ETSIDI::setTextColor(255, 255, 255);
+	ETSIDI::setFont("fuentes/Bitwise.ttf", 20);
+	ETSIDI::printxy(" 2 ", -10.5, 3.5);
+	ETSIDI::setTextColor(255, 255, 255);
+	ETSIDI::setFont("fuentes/Bitwise.ttf", 20);
+	ETSIDI::printxy(" 1   2   3   4   5   6    7   8", -8, -2);
+	ETSIDI::setTextColor(255, 255, 255);
+	ETSIDI::setFont("fuentes/Bitwise.ttf", 20);
+	ETSIDI::printxy(" 3 ", -10.5, 6.5);
+	ETSIDI::setTextColor(255, 255, 255);
+	ETSIDI::setFont("fuentes/Bitwise.ttf", 20);
+	ETSIDI::printxy(" 4  ", -10.5, 9.5);
+	ETSIDI::setTextColor(255, 255, 255);
+	ETSIDI::setFont("fuentes/Bitwise.ttf", 20);
+	ETSIDI::printxy(" 5 ", -10.5, 12.5);
+	ETSIDI::setTextColor(255, 255, 255);
+	ETSIDI::setFont("fuentes/Bitwise.ttf", 20);
+	ETSIDI::printxy(" 6  ", -10.5, 15.5);
+	ETSIDI::setTextColor(255, 255, 255);
+	ETSIDI::setFont("fuentes/Bitwise.ttf", 20);
+	ETSIDI::printxy(" 7 ", -10.5, 18.5);
+	ETSIDI::setTextColor(255, 255, 255);
+	ETSIDI::setFont("fuentes/Bitwise.ttf", 20);
+	ETSIDI::printxy(" 8", -10.5, 21.5);
 }
 
 int ListaPiezas::switchca(Pieza* p, Vector fin) {
