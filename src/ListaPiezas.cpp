b8e81e0d667c78f4556bc8d73b68151ffbaf5977
Asiermino <100568445+Asiermino@users.noreply.github.com>
2022-06-29 10:29:17 +0200

    Update ListaPiezas.cpp

--- src/ListaPiezas.cpp
+++ src/ListaPiezas.cpp
@@ -22,4 +22,25 @@ bool ListaPiezas::agregar(Pieza* p)
     return false;
   return true;
 }
-    
+
+void ListaPiezas::dibuja(){
+  for(int i=0; i<numero; i++){
+    lista[i]->dibuja();
+  }
+}
+
+void ListaPiezas::mueve(Vector ini, Vector fin){
+  for (int i=0; i<numero; i++)
+    lista[i]->mueve(ini, fin);
+}
+
+Pieza* ListaPiezas::comprobar(Vector pos){
+  for(int i=0; i<32; i++){
+    if (lista[i]->getpos()=pos)
+      return lista[i];
+  }
+}
+
+void ListaPiezas::switchca(Pieza* p){
+  
+}
