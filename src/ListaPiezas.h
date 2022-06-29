2efed2592d8e5780267c4bd6f12ff0917031a5d5
martaseisdedos <100596598+martaseisdedos@users.noreply.github.com>
2022-06-29 14:02:11 +0200

    Update ListaPiezas.h

--- src/ListaPiezas.h
+++ src/ListaPiezas.h
@@ -8,20 +8,26 @@
 #include "reina.h"
 #include "torre.h"
 #include "Vector.h"
-
+#include <iostream>
 
 class ListaPiezas{
   
   Pieza* lista[MAX_PIEZAS];
   int numero;
+  int mover;
   
 public:
       ListaPiezas();
       bool agregar(Pieza* p);
       void dibuja();
-      void mueve(Vector ini, Vector fin);
+      void mueve(Vector fin, Pieza* p);
   
       Pieza* comprobar(Vector);
-      void switchc(Pieza* p);
+      void switchca(Pieza* p, Vector v);
+  	  void comerpeones(Pieza* p, Vector v);
+	    void inicializarturnos();
+	    void cambiarturnos();
+	    void comer(Pieza* p, Vector v); 
+	    int getmover();
   
 };
