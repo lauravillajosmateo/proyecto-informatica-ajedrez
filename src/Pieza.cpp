a44e96ca6a6bd40766b88a25205b85dc264fe3bf
Sergiocabo6 <99722502+Sergiocabo6@users.noreply.github.com>
2022-06-30 18:48:56 +0200

    Update Pieza.cpp

--- src/Pieza.cpp
+++ src/Pieza.cpp
@@ -34,3 +34,11 @@ void Pieza::setturno(int t)
 {
 	turno = t;
 }
+
+int Pieza::casillalibre(Vector v, ListaPiezas l) {
+	for (int i = 0; i < l.numero; i++) {
+		if (v == l.lista[i]->getpos() && l.lista[i]->getcolor() == color) //mismo equipo, no puede comer
+			return 0;
+	}
+	return 1; //aqui puede comer (movimiento correcto)
+}
