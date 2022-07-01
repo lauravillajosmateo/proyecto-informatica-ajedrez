f9d9c704f5e1eabc516b4ee3d64c6276fa3ae7f4
evagombar <99722831+evagombar@users.noreply.github.com>
2022-07-01 12:16:24 +0200

    Update alfil.cpp

--- src/alfil.cpp
+++ src/alfil.cpp
@@ -5,18 +5,14 @@
 using namespace std;
 
 
-alfil::alfil (Vector palfil, int c) {
-
-	origen = palfil;
-	color = c;
-
-}
-
 void alfil::dibuja() {
 	
 	glTranslatef(origen.x, origen.y, 0);
 	glEnable(GL_TEXTURE_2D);
-	glBindTexture(GL_TEXTURE_2D, ETSIDI::getTexture("imagenes/alfilB.png").id);
+	if(color==BLANCO)
+		glBindTexture(GL_TEXTURE_2D, ETSIDI::getTexture("imagenes/alfilB.png").id);
+	else
+		glBindTexture(GL_TEXTURE_2D, ETSIDI::getTexture("imagenes/alfilN.png").id);
 	glDisable(GL_LIGHTING);
 	glBegin(GL_POLYGON);
 	glColor3f(1, 1, 1);
@@ -34,30 +30,119 @@ void alfil::dibuja() {
 
 
 
-int alfil::mov_correcto(Vector v)
+int alfil::mov_correcto(Vector v,ListaPiezas l)
 {
+	Vector aux = origen;
+	int contador = 0;
+	//primero se verifica que la posicion es valida por la naturaleza de la pieza
 	if ((v.x > origen.x) && (v.y < origen.y) || (v.x < origen.x) && (v.y > origen.y)) {
 		if (v.x + v.y == origen.x + origen.y) {
-			return MOV_CORRECTO;
+			contador++;
 		}
 		else
 			return ERROR;
 	}
 	else if ((v.x < origen.x) && (v.y < origen.y) || (v.x > origen.x) && (v.y > origen.y)) {
 		if (v.y - v.x == origen.y - origen.x) {
-			return MOV_CORRECTO;
+			contador++;
 		}
 		else
 			return ERROR;
 	}
 	else
 		return ERROR;
+
+	//arriba derecha
+
+	if (v.x > origen.x && v.y > origen.y)
+	{
+		do
+		{
+			for (int i = 0; i < l.numero; i++)
+			{
+				if (aux == l.lista[i]->getpos())
+				{
+					mover = 0;
+				}
+			}
+
+			aux.y++;
+			aux.x++;
+		} while (aux.x != v.x || aux.y != v.y);
+	}
+
+
+	//arriba izquierda
+	if (v.x < origen.x && v.y > origen.y)
+	{
+		do
+		{
+			for (int i = 0; i < l.numero; i++)
+			{
+				if (aux == l.lista[i]->getpos())
+				{
+					mover = 0;
+				}
+			}
+
+			aux.y++;
+			aux.x--;
+		} while (aux.x != v.x || aux.y != v.y);
+	}
+
+	//abajo derecha
+
+	if (v.x > origen.x && v.y < origen.y)
+	{
+		do
+		{
+			for (int i = 0; i < l.numero; i++)
+			{
+				if (aux == l.lista[i]->getpos())
+				{
+					mover = 0;
+				}
+			}
+
+			aux.y--;
+			aux.x++;
+		} while (aux.x != v.x || aux.y != v.y);
+	}
+
+	//abajo izquierda
+	if (v.x < origen.x && v.y < origen.y)
+	{
+		do
+		{
+			for (int i = 0; i < l.numero; i++)
+			{
+				if (aux == l.lista[i]->getpos())
+				{
+					mover = 0;
+				}
+			}
+
+			aux.y--;
+			aux.x--;
+		} while (aux.x != v.x || aux.y != v.y);
+	}
+
+	if (mover == 0)
+		return ERROR;
+	else
+		contador++;
+
+	if (Pieza::casillalibre(v, l) == 1)
+		contador++;
+
+	if (contador == 3)
+		return MOV_CORRECTO;
+
 }
 
-void alfil::movimientos(Vector v)
+void alfil::movimientos(Vector v,ListaPiezas l)
 {
-	if (mov_correcto(v) == 1 && turno==0) {
-	if (mov_correcto(v) == 1) {
+	if (mov_correcto(v,l) == 1 && turno==0) {
 
 		if (v.x > origen.x && v.y > origen.y) {
 			do { //movimiento arriba-derecha
@@ -98,11 +183,7 @@ void alfil::movimientos(Vector v)
 
 		turno = 1;
 	}
-	}
 	else
 		cout << "MOVIMIENTO INCORRECTO. Prueba otra vez." << endl;
-	
 }
-
-
 
