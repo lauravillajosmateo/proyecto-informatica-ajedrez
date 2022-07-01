a366f4785144f18e746fa41b01a392036f872b76
evagombar <99722831+evagombar@users.noreply.github.com>
2022-07-01 12:18:33 +0200

    Update rey.cpp

--- src/rey.cpp
+++ src/rey.cpp
@@ -5,16 +5,11 @@
 
 using namespace std;
 
-rey::rey(Vector prey, int c) {
-	origen=prey;
-	color=c;
-}
-
 
 void rey::dibuja() {
 	glTranslatef(origen.x, origen.y, 0.01);
 	glEnable(GL_TEXTURE_2D);
-	if(COLOR==BLANCO);
+	if (color == BLANCO)
 		glBindTexture(GL_TEXTURE_2D, ETSIDI::getTexture("imagenes/reyB.png").id);
 	else
 		glBindTexture(GL_TEXTURE_2D, ETSIDI::getTexture("imagenes/reyN.png").id);
@@ -32,74 +27,84 @@ void rey::dibuja() {
 }
 
 
-int rey::mov_correcto(Vector v)
+int rey::mov_correcto(Vector v,ListaPiezas l)
 {
-	
+	Vector aux = origen;
+	int contador = 0;
+
 	if (v.y + 1 == origen.y) // si el destino es hacia arriba
 		if (v.x + 1 == origen.x) // y en diagonal izquierda
-			return MOV_CORRECTO;
+			contador++;
 		else if (v.x == origen.x)//misma columna
-			return MOV_CORRECTO;
+			contador++;
 		else if (v.x - 1 == origen.x) // en diagonal derecha
-			return MOV_CORRECTO;
+			contador++;
 		else
 			return ERROR;
 	else if (v.y == origen.y) //si el destino es en la misma fila
 		if (v.x + 1 == origen.x) //hacia la izquierda
-			return MOV_CORRECTO;
+			contador++;
 		else if (v.x - 1 == origen.x)//hacia la derecha
-			return MOV_CORRECTO;
+			contador++;
 		else
 			return ERROR;
 	else if (v.y - 1 == origen.y) //si el destino es hacia abajo
-		if (v.x + 1 == origen.x)//hacia la izquierda
-			return MOV_CORRECTO;
-		else if (v.x == origen.x)
-			return MOV_CORRECTO;
-		else if (v.x - 1 == origen.x)
-			return MOV_CORRECTO;
-		else
-			return ERROR;
+			if (v.x + 1 == origen.x)//hacia la izquierda
+			contador++;
+			else if (v.x == origen.x)
+				contador++;
+				else if (v.x - 1 == origen.x)
+						contador++;
+						else
+							return ERROR;
+	else
+		return ERROR;
+
+	if (Pieza::casillalibre(v, l) == 1)
+		contador++;
+
+	if (contador == 2)
+		return MOV_CORRECTO;
 	else
 		return ERROR;
 
 }
 
 
-void rey::movimientos(Vector v)
+void rey::movimientos(Vector v,ListaPiezas l)
 {
-	if (mov_correcto(v) == 1 && turno == 0) {
-	if (mov_correcto(v) == 1) {
-		turno = 1;
-		if ((v.x == origen.x) && (v.y > origen.y))//mover arriba
-			origen.y = origen.y + 1;
-		else if ((v.x == origen.x) && (v.y < origen.y))//mover abajo
-			origen.y = origen.y - 1;
+	if (mov_correcto(v,l) == 1 && turno == 0) {
+		if (mov_correcto(v,l) == 1) {
+			turno = 1;
+			if ((v.x == origen.x) && (v.y > origen.y))//mover arriba
+				origen.y = origen.y + 1;
+			else if ((v.x == origen.x) && (v.y < origen.y))//mover abajo
+				origen.y = origen.y - 1;
 
-		else if ((v.x > origen.x) && (v.y == origen.y))
-			origen.x = origen.x + 1;//mover derecha
+			else if ((v.x > origen.x) && (v.y == origen.y))
+				origen.x = origen.x + 1;//mover derecha
 
-		else if ((v.x < origen.x) && (v.y == origen.y))//mover izquierda
-			origen.x = origen.x - 1;
-		else if ((v.x > origen.x) && (v.y > origen.y)) {//arriba-derecha
-			origen.x = origen.x + 1;
-			origen.y = origen.y + 1;
-		}
-		else if ((v.x > origen.x) && (v.y< origen.y)) {//abajo-derecha
-			origen.x = origen.x + 1;
-			origen.y = origen.y - 1;
+			else if ((v.x < origen.x) && (v.y == origen.y))//mover izquierda
+				origen.x = origen.x - 1;
+			else if ((v.x > origen.x) && (v.y > origen.y)) {//arriba-derecha
+				origen.x = origen.x + 1;
+				origen.y = origen.y + 1;
+			}
+			else if ((v.x > origen.x) && (v.y < origen.y)) {//abajo-derecha
+				origen.x = origen.x + 1;
+				origen.y = origen.y - 1;
 
-		}
-		else if ((v.x > origen.x) && (v.y > origen.y)) {//arriba-izquierda
-			origen.x = origen.x - 1;
-			origen.y = origen.y + 1;
-		}
-		else if ((v.x < origen.x) && (v.y < origen.y)) {//abajo-izquierda
+			}
+			else if ((v.x > origen.x) && (v.y > origen.y)) {//arriba-izquierda
+				origen.x = origen.x - 1;
+				origen.y = origen.y + 1;
+			}
+			else if ((v.x < origen.x) && (v.y < origen.y)) {//abajo-izquierda
 
-			origen.x = origen.x - 1;
-			origen.y = origen.y - 1;
+				origen.x = origen.x - 1;
+				origen.y = origen.y - 1;
+			}
 		}
-	}
 	}
 	else
 		cout << "MOVIMIENTO INCORRECTO. Prueba otra vez." << endl;
