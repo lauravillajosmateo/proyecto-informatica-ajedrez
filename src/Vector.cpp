e2230cc5621cc538f2df6832b9b68d57d12d5897
martaseisdedos <100596598+martaseisdedos@users.noreply.github.com>
2022-07-06 14:26:25 +0200

    Update Vector.cpp

--- src/Vector.cpp
+++ src/Vector.cpp
@@ -1,15 +1,19 @@
 #include "Vector.h"
 
-
-
-
 Vector::Vector()
 {
 	x = 0;
 	y = 0;
 }
 
-bool Vector::operator=(Vector vector)
+Vector::Vector(int posx, int posy)
+{
+	x = posx;
+	y = posy;
+}
+
+
+bool Vector::operator==(const Vector& vector)
 {
 	if (vector.x == x && vector.y == y)
 	{
@@ -19,3 +23,23 @@ bool Vector::operator=(Vector vector)
 	else
 		return false;
 }
+
+bool Vector::operator!=(const Vector& vector)
+{
+	if (vector.x != x || vector.y != y)
+	{
+		return true;
+
+	}
+	else
+		return false;
+}
+
+//Vector Vector:: operator+(Vector vector)
+//{
+//	Vector solucion;
+//	solucion.x = x + vector.x;
+//	solucion.y = y + vector.y;
+//
+//	return solucion;
+//}
