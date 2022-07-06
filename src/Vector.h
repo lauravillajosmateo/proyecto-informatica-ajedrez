0a691cc9c5b3533de36270eb7a2c20bde0168492
martaseisdedos <100596598+martaseisdedos@users.noreply.github.com>
2022-07-06 14:25:13 +0200

    Update Vector.h

--- src/Vector.h
+++ src/Vector.h
@@ -2,14 +2,14 @@
 
 class Vector
 {
-
 public:
 	int x;
 	int y;
 
 	Vector();
+	Vector(int posx, int posy);
 
-	bool operator = (Vector vector);
-
-
+	bool operator == (const Vector& vector);
+	//Vector operator+(Vector vector);
+	bool operator!=(const Vector& vector);
 };
