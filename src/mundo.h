adf2786d68ae364fe9d38f0e904dfbc470150da1
martaseisdedos <100596598+martaseisdedos@users.noreply.github.com>
2022-07-06 14:24:22 +0200

    Update mundo.h

--- src/mundo.h
+++ src/mundo.h
@@ -1,20 +1,13 @@
-#include "tablero.h"
-#include "peon.h"
-#include "peonn.h"
-#include "ListaPeonNegro.h"
-#include "ListaPeonBlanco.h"
-#include "rey.h"
-#include "reyN.h"
-#include "alfil.h"
-#include "alfilN.h"
-#include "caballo.h"
-#include "caballoN.h"
-#include "reina.h"
-#include "reinaN.h"
-#include "torre.h"
-#include "torreN.h"
-#include "marco.h"
 #include "cartel.h"
+#include "Pieza.h"
+#include "alfil.h"
+#include "caballo.h"
+#include "peon.h"
+#include "reina.h"
+#include "rey.h"
+#include "torre.h"
+
+#include "Vector.h"
 
 class mundo
 {
@@ -25,36 +18,12 @@ public:
 	int mover;
 	int carteltb;
 	int cartelcome;
-
-	
-
-	ListaPeonNegro listapeonn; //hay 8 peones negros
-	ListaPeonBlanco listapeonb; //hay 8 peones blancos
-	rey reyb;
-	reyN reyn;
-
-	reina reinab;
-	reinaN reinan;
-
-	caballo caballob1;
-	caballo caballob2;
-	caballoN caballon1;
-	caballoN caballon2;
-
-	torre torreb1;
-	torre torreb2;
-	torreN torren1;
-	torreN torren2;
-
-	alfil alfilb1;
-	alfil alfilb2;
-	alfilN alfiln1;
-	alfilN alfiln2;
+	Vector posini, posfin;
 
 	Marco marco1;
-	Cartel cartel;
+	//Cartel cartel;
 
-
+	ListaPiezas listapiezas;
 
 
 	mundo();
@@ -63,11 +32,12 @@ public:
 	void inicializa();
 	void dibuja();
 	void mueve();
-	void tecla(unsigned char key);
-	void hayjaqueB();
+
+	//void tecla(unsigned char key);
+	/*void hayjaqueB();
 	void hayjaqueN();
 
-	int final();
+	int final();*/
 
 	tablero tablero1;
 
