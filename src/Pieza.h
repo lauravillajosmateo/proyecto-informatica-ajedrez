cbbf0b435615a220af87ff13b61bda5de923e5d0
lauravillajosmateo <48021437+lauravillajosmateo@users.noreply.github.com>
2022-07-06 16:20:55 +0200

    Update Pieza.h

--- src/Pieza.h
+++ src/Pieza.h
@@ -8,7 +8,7 @@
 using namespace ETSIDI;
 
 typedef enum {BLANCO=0, NEGRO};
-typedef enum { PEON = 1, TORRE, CABALLO, ALFIL, REINA, REY };
+
 
 
 class Pieza {
@@ -18,12 +18,11 @@ protected:
 
 	int turno;
 	int color;
-	int mover;
-	int marca;
+	bool marca_rey;
 
 public:
-	Pieza():origen(0,0),turno(0), marca(0), color(0), mover(1){}
-	Pieza(Vector v, int c, int m):origen(v), color(c), marca(m){}
+	Pieza():origen(0,0),turno(0),marca_rey(false), color (BLANCO) {}
+	Pieza(Vector v, int c, bool m):origen(v),color(c),marca_rey(m){}
 	
 	
 	void setpos(Vector posicion);
@@ -32,8 +31,8 @@ public:
 
 	virtual void dibuja() = 0;
 /*void mueve(Vector inicio, Vector final);*/
-virtual int mov_correcto(Vector, ListaPiezas ) = 0; //realiza todas las comprobaciones
-virtual void movimientos(Vector,ListaPiezas) = 0; //esta función realiza el movimiento una vez se han realizado todas las comprobaciones y es el
+virtual int mov_correcto(Vector, ListaPiezas& ) = 0; //realiza todas las comprobaciones
+virtual void movimientos(Vector,ListaPiezas&) = 0; //esta función realiza el movimiento una vez se han realizado todas las comprobaciones y es el
 //turno correspondiente.
 
 int getturno();
@@ -41,7 +40,7 @@ int getmarca();
 int getcolor();
 void setturno(int t);
 
-int casillalibre(Vector v, ListaPiezas l);
-
+int casillalibre(Vector v, ListaPiezas& l);
+void piezacomida(Vector v, ListaPiezas& l);
 
 };
