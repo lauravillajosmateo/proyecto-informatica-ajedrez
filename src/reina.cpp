d10b4230e9b1e7dd00847990fb8c527773fa4d0b
lauravillajosmateo <48021437+lauravillajosmateo@users.noreply.github.com>
2022-07-06 16:10:17 +0200

    Update reina.cpp

--- src/reina.cpp
+++ src/reina.cpp
@@ -4,11 +4,6 @@
 
 using namespace std;
 
-reina::reina(Vector preina, int c) {
-    origen=preina;
-    color=c;
-}
-
 void reina::dibuja() {
 
     glTranslatef(origen.x-0.5, origen.y-0.9, 0);
@@ -37,6 +32,7 @@ int reina::mov_correcto(Vector v, ListaPiezas l)
 {
     Vector aux = origen;
     int contador=0;
+	int mover=1;
     //parte de la torre
     if ((v.x == origen.x) && (v.y == origen.y)) {
         return ERROR;
@@ -70,82 +66,82 @@ int reina::mov_correcto(Vector v, ListaPiezas l)
         
         //hacia arriba
 
-		if (v.x == p->getpos().x && v.y > p->getpos().y)
+		if (v.x == origen.x && v.y > origen.y)
+	{
+		do
 		{
-			do
-			{
-				for (int i = 0; i < l.numero; i++) {
-					if (aux == l.lista[i]->getpos())
-					{
-						mover = 0;
-					}
-
+			for (int i = 0; i < l.numero; i++) {
+				if (aux.y + 1 == l.lista[i]->getpos().y && aux.x == l.lista[i]->getpos().x)
+				{
+					mover = 0;
 				}
 
-				aux.y++;
-			} while (aux.y != v.y);//mover 0 es que NO se puede mover
-		}
+			}
+
+			aux.y++;
+		} while (aux.y != v.y);//mover 0 es que NO se puede mover
+	}
 
 		//hacia izquierda
-		if (v.x < p->getpos().x && v.y == p->getpos().y)
+			if (v.x < origen.x && v.y == origen.y)
+	{
+		do
 		{
-			do
-			{
-				for (int i = 0; i < l.numero; i++) {
-					if (aux == l.lista[i]->getpos())
-					{
-						mover = 0;
-					}
-
+			for (int i = 0; i < l.numero; i++) {
+				if (aux.x - 1 == l.lista[i]->getpos().x && aux.y == l.lista[i]->getpos().y)
+				{
+					mover = 0;
 				}
 
-				aux.x--;
-			} while (aux.x != v.x);//mover 0 es que NO se puede mover
-		}
+			}
+
+			aux.x--;
+		} while (aux.x != v.x);//mover 0 es que NO se puede mover
+	}
 
 		//hacia derecha
-		if (v.x > p->getpos().x && v.y == p->getpos().y)
+			if (v.x > origen.x && v.y == origen.y)
+	{
+		do
 		{
-			do
-			{
-				for (int i = 0; i < l.numero; i++) {
-					if (aux == l.lista[i]->getpos())
-					{
-						mover = 0;
-					}
-
+			for (int i = 0; i < l.numero; i++) {
+				if (aux.x + 1 == l.lista[i]->getpos().x && aux.y == l.lista[i]->getpos().y)
+				{
+					mover = 0;
 				}
 
-				aux.x++;
-			} while (aux.x != v.x);//mover 0 es que NO se puede mover
-		}
+			}
+
+			aux.x++;
+		} while (aux.x != v.x);//mover 0 es que NO se puede mover
+	}
 
 		//hacia abajo
 
-		if (v.x == p->getpos().x && v.y < p->getpos().y)
+		if (v.x == origen.x && v.y < origen.y)
+	{
+		do
 		{
-			do
-			{
-				for (int i = 0; i < l.numero; i++) {
-					if (aux == l.lista[i]->getpos())
-					{
-						mover = 0;
-					}
-
+			for (int i = 0; i < l.numero; i++) {
+				if (aux.y - 1 == l.lista[i]->getpos().y && aux.x == l.lista[i]->getpos().x)
+				{
+					mover = 0;
 				}
 
-				aux.y--;
-			} while (aux.y != v.y);//mover 0 es que NO se puede mover
-		}
+			}
+
+			aux.y--;
+		} while (aux.y != v.y);//mover 0 es que NO se puede mover
+	}
 		//arriba derecha
 
-		if (v.x > p->getpos().x && v.y > p->getpos().y)
+		if (v.x > origen.x && v.y > origen.y)
 		{
 			do
 			{
 				for (int i = 0; i < l.numero; i++)
 				{
-					if (aux == l.lista[i]->getpos())
+					if (aux.x + 1 == l.lista[i]->getpos().x && aux.y + 1 == l.lista[i]->getpos().y)
 					{
 						mover = 0;
 					}
@@ -158,13 +154,13 @@ int reina::mov_correcto(Vector v, ListaPiezas l)
 
 
 		//arriba izquierda
-		if (v.x < p->getpos().x && v.y > p->getpos().y)
+		if (v.x < origen.x && v.y > origen.y)
 		{
 			do
 			{
 				for (int i = 0; i < l.numero; i++)
 				{
-					if (aux == l.lista[i]->getpos())
+					if (aux.x - 1 == l.lista[i]->getpos().x && aux.y + 1 == l.lista[i]->getpos().y)
 					{
 						mover = 0;
 					}
@@ -177,13 +173,13 @@ int reina::mov_correcto(Vector v, ListaPiezas l)
 
 		//abajo derecha
 
-		if (v.x > p->getpos().x && v.y < p->getpos().y)
+		if (v.x > origen.x && v.y < origen.y)
 		{
 			do
 			{
 				for (int i = 0; i < l.numero; i++)
 				{
-					if (aux == l.lista[i]->getpos())
+					if (aux.x + 1 == l.lista[i]->getpos().x && aux.y - 1 == l.lista[i]->getpos().y)
 					{
 						mover = 0;
 					}
@@ -195,13 +191,13 @@ int reina::mov_correcto(Vector v, ListaPiezas l)
 		}
 
 		//abajo izquierda
-		if (v.x < p->getpos().x && v.y < p->getpos().y)
+		if (v.x < origen.x && v.y < origen.y)
 		{
 			do
 			{
 				for (int i = 0; i < l.numero; i++)
 				{
-					if (aux == l.lista[i]->getpos())
+					if (aux.x - 1 == l.lista[i]->getpos().x && aux.y - 1 == l.lista[i]->getpos().y)
 					{
 						mover = 0;
 					}
@@ -210,95 +206,104 @@ int reina::mov_correcto(Vector v, ListaPiezas l)
 				aux.y--;
 				aux.x--;
 			} while (aux.x != v.x || aux.y != v.y);
-        }
+		}
         
       if (mover == 0)
         return ERROR;
     else
         contador++;
     
-    if (Pieza::casillalibre(v, l) == 1)
-        contador++;
+   if (contador == 2 && Pieza::casillalibre(v, l) == 1) {
 
-    if (contador == 3)
-        return MOV_CORRECTO;    
-	
-	else
+		Pieza::piezacomida(v, l);
+		return MOV_CORRECTO;
+	}
+
+	if (contador == 2 && Pieza::casillalibre(v, l) == 2) {
+
+		return MOV_CORRECTO;
+	}
+
+	if(Pieza::casillalibre(v, l) == 0)
 		return ERROR;
 }
 
-void reina::movimientos(Vector v)
+void reina::movimientos(Vector v,ListaPiezas& l)
 {
-    //parte torre
-    if (mov_correcto(v) == 1 && turno==0) {
+	if (mov_correcto(v,l) == 1 && turno == 0) {
 
-        turno = 1;
-        if ((v.x == origen.x) && (v.y > origen.y))//mover arriba
-        {
-            do {
-                origen.y = origen.y + 1;
-            } while (origen.y != v.y);
-        }
-        else if ((v.x == origen.x) && (v.y < origen.y))//mover abajo
-        {
-            do {
-                origen.y = origen.y - 1;
-            } while (origen.y != v.y);
-        }
-        else if ((v.x > origen.x) && (v.y == origen.y))//mover derecha
-        {
-            do {
-                origen.x = origen.x + 1;
-            } while (origen.x != v.x);
-        }
-        else if ((v.x < origen.x) && (v.y == origen.y))//mover izquierda
-        {
-            do {
-                origen.x = origen.x - 1;
-            } while (origen.x != v.x);
-        }
+		turno = 2;
+		if ((v.x == origen.x) && (v.y > origen.y))//mover arriba
+		{
+			do {
+				origen.y = origen.y + 1;
+			} while (origen.y != v.y);
+		}
+		if ((v.x == origen.x) && (v.y < origen.y))//mover abajo
+		{
+			do {
+				origen.y = origen.y - 1;
+			} while (origen.y != v.y);
+		}
+		if ((v.x > origen.x) && (v.y == origen.y))//mover derecha
+		{
+			do {
+				origen.x = origen.x + 1;
+			} while (origen.x != v.x);
+		}
+		if ((v.x < origen.x) && (v.y == origen.y))//mover izquierda
+		{
+			do {
+				origen.x = origen.x - 1;
+			} while (origen.x != v.x);
+		}
 
-        //parte alfil
+		//parte alfil
 
-        if (v.x > origen.x && v.y > origen.y) {
-            do { //movimiento arriba-derecha
+		if (v.x > origen.x && v.y > origen.y) {
+			do { //movimiento arriba-derecha
 
-                origen.x = origen.x + 1;
-                origen.y = origen.y + 1;
+				origen.x = origen.x + 1;
+				origen.y = origen.y + 1;
 
-            } while (origen.x != v.x && origen.y != v.y);
-        }
+			} while (origen.x != v.x && origen.y != v.y);
+		}
 
-        else if (v.x < origen.x && v.y < origen.y) {
-            do { //movimiento abajo-izquierda
+		if (v.x < origen.x && v.y < origen.y) {
+			do { //movimiento abajo-izquierda
 
-                origen.x = origen.x - 1;
-                origen.y = origen.y - 1;
+				origen.x = origen.x - 1;
+				origen.y = origen.y - 1;
 
 
-            } while (origen.x != v.x && origen.y != v.y);
-        }
+			} while (origen.x != v.x && origen.y != v.y);
+		}
 
-        else if (v.x < origen.x && v.y > origen.y) {
-            do { //arriba-izquierda
+		if (v.x < origen.x && v.y > origen.y) {
+			do { //arriba-izquierda
 
-                origen.x = origen.x - 1; 
-                origen.y = origen.y + 1;
+				origen.x = origen.x - 1;
+				origen.y = origen.y + 1;
 
-            } while (origen.x != v.x && origen.y != v.y);
-        }
+			} while (origen.x != v.x && origen.y != v.y);
+		}
 
-        else if (v.x > origen.x && v.y < origen.y) {
-            do { //abajo-derecha
+		if (v.x > origen.x && v.y < origen.y) {
+			do { //abajo-derecha
 
-                origen.x = origen.x + 1; 
-                origen.y = origen.y - 1;
+				origen.x = origen.x + 1;
+				origen.y = origen.y - 1;
 
-            } while (origen.x != v.x && origen.y != v.y);
-        }
-    }
-}
-    else
-        cout << "MOVIMIENTO INCORRECTO. Prueba otra vez." << endl;
+			} while (origen.x != v.x && origen.y != v.y);
+		}
+
+		for (int i = 0; i < l.numero; i++) {
+			if (l.lista[i]->getmarca() == true)
+				l.lista[i]->hayjaque(l);
+		}
+	}
+
+ else
+	cout << "MOVIMIENTO INCORRECTO. Prueba otra vez." << endl;
 }
 
