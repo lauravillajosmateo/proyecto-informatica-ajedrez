5394bbc5b3f0c70cd44a019fb3f5ac8b903b0018
lauravillajosmateo <48021437+lauravillajosmateo@users.noreply.github.com>
2022-07-06 16:17:18 +0200

    Update Pieza.cpp

--- src/Pieza.cpp
+++ src/Pieza.cpp
@@ -10,7 +10,18 @@ Vector Pieza::getpos()
 }
 
 
+void Pieza::hayjaque(ListaPiezas& l)
+{
+	for (int i = 0; i < l.numero; i++) {
+		if (l.lista[i]->getcolor() != color && l.lista[i]->mov_correcto(origen, l)) {
+			if (color == BLANCO)
+				cout << "Hay jaque para blancas" << endl << endl;
+			else
+				cout << "Hay jaque para negras" << endl << endl;
+		}
 
+	}
+}
 int Pieza::getturno()
 {
 	return turno;
@@ -20,11 +31,33 @@ void Pieza::setturno(int t)
 {
 	turno = t;
 }
+bool Pieza::getmarca()
+{
+	return marca_rey;
+}
 
-int Pieza::casillalibre(Vector v, ListaPiezas l) {
+int Pieza::getcolor()
+{
+	return color;
+}
+
+int Pieza::casillalibre(Vector v, ListaPiezas& l) {
 	for (int i = 0; i < l.numero; i++) {
-		if (v == l.lista[i]->getpos() && l.lista[i]->getcolor() == color) //mismo equipo, no puede comer
-			return 0;
+		if (v == l.lista[i]->getpos() && l.lista[i]->getcolor() != color) //distinto equipo, puede comer
+			return 1;
+		if (v != l.lista[i]->getpos()) //casilla libre
+			return 2;
 	}
-	return 1; //aqui puede comer (movimiento correcto)
+	return 0; //no se puede mover, casilla ocupada por una de su color
+}
+
+void Pieza::piezacomida(Vector v, ListaPiezas& l) {
+	for (int i = 0; i < l.numero; i++) {
+		if (v == l.lista[i]->getpos() && l.lista[i]->getcolor() != color)
+		{
+			Vector pos(20, 20);
+			l.lista[i]->setpos(pos);
+		}
+	}
+
 }
