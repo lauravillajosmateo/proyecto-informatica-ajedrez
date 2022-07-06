9499d192d40b77b0bed3fba51966c18e09f5ac57
lauravillajosmateo <48021437+lauravillajosmateo@users.noreply.github.com>
2022-07-06 15:56:32 +0200

    Update ListaPiezas.cpp

--- src/ListaPiezas.cpp
+++ src/ListaPiezas.cpp
@@ -9,7 +9,7 @@ ListaPiezas::ListaPiezas(){
     lista[i]=0;
   }
   numero=0;
-  mover=1;
+ 
 }
 
 bool ListaPiezas::agregar(Pieza* p)
@@ -33,42 +33,19 @@ void ListaPiezas::dibuja(){
   }
 }
 
-void ListaPiezas::mueve(Vector fin, Pieza* p){
-  p->movimientos(fin);
-  p->setavanza(0);
-  p->setcome(0);
-}
 
 Pieza* ListaPiezas::comprobar(Vector pos){ //comprobar posicion inicial
-  for(int i=0; i<32; i++){
-    if (lista[i]->getpos()=pos)
+  for(int i=0; i<nummero; i++){
+    if (lista[i]->getpos()==pos)
       return lista[i];
   }
+	return NULL;
 }
 
-void ListaPiezas::comerpeones(Pieza* p, Vector v) //comprobar come y avanza. Le pasamos la pieza del peon que se va a mover y el vector de la posicion final a la que se mueve
-{
-	for (int i = 0; i < 32; i++)
-	{
-		if (p->getmarca() == 1 && p->getcolor()==0  && lista[i]->getcolor() == 1 && (lista[i]->getpos() = v))
-		{
-			p->setcome(1);
-		}
-		else if (p->getmarca() == 1 && p->getcolor() == 1 && lista[i]->getcolor() == 0 && (lista[i]->getpos() = v))
-		{
-			p->setcome(1);
-		}
-
-		else
-		{
-			p->setavanza(1);
-		}
-	}
-}
 
 void ListaPiezas::inicializarturnos() //para inicializar en el primer movimiento de la partida
 {
-	for (int i = 0; i < 32; i++)
+	for (int i = 0; i < numero; i++)
 	{
 		if (lista[i]->getcolor() == 0) // empiezan blancas porque estan a 0. Negras esperan
 		{
@@ -81,14 +58,14 @@ void ListaPiezas::inicializarturnos() //para inicializar en el primer movimiento
 
 void ListaPiezas::cambiarturnos()
 {
-	for (int i = 0; i < 32; i++)
+	for (int i = 0; i < numero; i++)
 	{
 		dibujatblancas();
-		if (lista[i]->getturno() == 1 && lista[i]->getcolor() == 0)//si una blanca se ha movido
+		if (lista[i]->getturno() == 2 && lista[i]->getcolor() == BLANCO)//si una blanca se ha movido
 		{
-			for (int j = 0; j < 32; j++)
+			for (int j = 0; j < numero; j++)
 			{
-				if (lista[j]->getcolor() == 0)
+				if (lista[j]->getcolor() == BLANCO)
 				{
 					lista[j]->setturno(1);//se acaba turno blancas
 					
@@ -100,12 +77,13 @@ void ListaPiezas::cambiarturnos()
 			}
 		}
 
-		else if (lista[i]->getturno() == 1 && lista[i]->getcolor() == 1)
+		if (lista[i]->getturno() == 2 && lista[i]->getcolor() == NEGRO)
 		{
+			lista[i]->setturno(1);
 			dibujatnegras();
 			for (int j = 0; j < 32; j++)
 			{
-				if (lista[j]->getcolor() == 1)
+				if (lista[j]->getcolor() == NEGRO)
 				{
 					lista[j]->setturno(1);//se acaba turno negras
 
@@ -120,26 +98,7 @@ void ListaPiezas::cambiarturnos()
 	}
 }
 
-void ListaPiezas::comer(Pieza* p, Vector v) //vector de posicion final
-{											//comprueba que una pieza se puede comer y la manda al cementerio de piezas
-	for (int i = 0; i < 32; i++)
-	{
-		if (p->getcolor() == 1 && (lista[i]->getpos() = v) && lista[i]->getcolor()==0 && lista[i]->getturno()==1)
-		{
-			Vector x(11, 3);
-			lista[i]->setpos(x);
-			cout << "Tu pieza ha sido comida" << endl << endl;
-		}
 
-		else if (p->getcolor() == 0 && (lista[i]->getpos() = v) && lista[i]->getcolor() == 1 && lista[i]->getturno() == 1)
-		{
-			Vector y(-3, 3);
-			lista[i]->setpos(y);
-			cout << "Tu pieza ha sido comida" << endl << endl;
-		}
-	}
-
-}
 
 void ListaPiezas::dibujatblancas()
 {
@@ -215,341 +174,8 @@ void ListaPiezas::dibujatnegras()
 	ETSIDI::printxy(" 8", -10.5, 21.5);
 }
 
-int ListaPiezas::switchca(Pieza* p, Vector fin) {
 
-	Vector aux = p->getpos();
-
-	switch (p->getmarca())
-	{
-	case 0: 
-			mover = 1;
-			break;
-
-	case 1: 
-		mover = 1;
-		break;
-
-	case 2: { //TORRE
-
-		//hacia arriba
-		
-			if (fin.x == p->getpos().x && fin.y > p->getpos().y)
-			{
-				do
-				{
-					for (int i = 0; i < 32; i++) {
-						if (aux = lista[i]->getpos())
-						{
-							mover = 0;
-						}
-						
-					}
-
-					aux.y++; 
-				} while (aux.y != fin.y);//mover 0 es que NO se puede mover
-			}
-
-		//hacia izquierda
-			if (fin.x < p->getpos().x && fin.y == p->getpos().y)
-			{
-				do
-				{
-					for (int i = 0; i < 32; i++) {
-						if (aux = lista[i]->getpos())
-						{
-							mover = 0;
-						}
-
-					}
-
-					aux.x--;
-				} while (aux.x != fin.x);//mover 0 es que NO se puede mover
-			}
-
-			//hacia derecha
-			if (fin.x > p->getpos().x && fin.y == p->getpos().y)
-			{
-				do
-				{
-					for (int i = 0; i < 32; i++) {
-						if (aux = lista[i]->getpos())
-						{
-							mover = 0;
-						}
-
-					}
-
-					aux.x++;
-				} while (aux.x != fin.x);//mover 0 es que NO se puede mover
-			}
-
-			//hacia abajo
-
-			if (fin.x == p->getpos().x && fin.y < p->getpos().y)
-			{
-				do
-				{
-					for (int i = 0; i < 32; i++) {
-						if (aux = lista[i]->getpos())
-						{
-							mover = 0;
-						}
-
-					}
-
-					aux.y--;
-				} while (aux.y != fin.y);//mover 0 es que NO se puede mover
-			}
-			break;
-		
-				
-	}
-
-	case 3: //caballo
-		mover = 1;
-		break;
-
-	case 4: //alfil
-	{
-		//arriba derecha
-
-		if (fin.x > p->getpos().x && fin.y > p->getpos().y)
-		{
-			do
-			{
-				for (int i = 0; i < 32; i++)
-				{
-					if (aux = lista[i]->getpos())
-					{
-						mover = 0;
-					}
-				}
-
-				aux.y++;
-				aux.x++;
-			} while (aux.x != fin.x || aux.y != fin.y);
-		}
-
-
-		//arriba izquierda
-		if (fin.x < p->getpos().x && fin.y > p->getpos().y)
-		{
-			do
-			{
-				for (int i = 0; i < 32; i++)
-				{
-					if (aux = lista[i]->getpos())
-					{
-						mover = 0;
-					}
-				}
-
-				aux.y++;
-				aux.x--;
-			} while (aux.x != fin.x || aux.y != fin.y);
-		}
-
-		//abajo derecha
-
-		if (fin.x > p->getpos().x && fin.y < p->getpos().y)
-		{
-			do
-			{
-				for (int i = 0; i < 32; i++)
-				{
-					if (aux = lista[i]->getpos())
-					{
-						mover = 0;
-					}
-				}
-
-				aux.y--;
-				aux.x++;
-			} while (aux.x != fin.x || aux.y != fin.y);
-		}
-
-		//abajo izquierda
-		if (fin.x < p->getpos().x && fin.y < p->getpos().y)
-		{
-			do
-			{
-				for (int i = 0; i < 32; i++)
-				{
-					if (aux = lista[i]->getpos())
-					{
-						mover = 0;
-					}
-				}
-
-				aux.y--;
-				aux.x--;
-			} while (aux.x != fin.x || aux.y != fin.y);
-		}
-
-		
-		break;
-	}
-
-	case 5: {//reina
-
-		//hacia arriba
-
-		if (fin.x == p->getpos().x && fin.y > p->getpos().y)
-		{
-			do
-			{
-				for (int i = 0; i < 32; i++) {
-					if (aux = lista[i]->getpos())
-					{
-						mover = 0;
-					}
-
-				}
-
-				aux.y++;
-			} while (aux.y != fin.y);//mover 0 es que NO se puede mover
-		}
-
-		//hacia izquierda
-		if (fin.x < p->getpos().x && fin.y == p->getpos().y)
-		{
-			do
-			{
-				for (int i = 0; i < 32; i++) {
-					if (aux = lista[i]->getpos())
-					{
-						mover = 0;
-					}
-
-				}
-
-				aux.x--;
-			} while (aux.x != fin.x);//mover 0 es que NO se puede mover
-		}
-
-		//hacia derecha
-		if (fin.x > p->getpos().x && fin.y == p->getpos().y)
-		{
-			do
-			{
-				for (int i = 0; i < 32; i++) {
-					if (aux = lista[i]->getpos())
-					{
-						mover = 0;
-					}
-
-				}
-
-				aux.x++;
-			} while (aux.x != fin.x);//mover 0 es que NO se puede mover
-		}
-
-		//hacia abajo
-
-		if (fin.x == p->getpos().x && fin.y < p->getpos().y)
-		{
-			do
-			{
-				for (int i = 0; i < 32; i++) {
-					if (aux = lista[i]->getpos())
-					{
-						mover = 0;
-					}
-
-				}
-
-				aux.y--;
-			} while (aux.y != fin.y);//mover 0 es que NO se puede mover
-		}
-		//arriba derecha
-
-		if (fin.x > p->getpos().x && fin.y > p->getpos().y)
-		{
-			do
-			{
-				for (int i = 0; i < 32; i++)
-				{
-					if (aux = lista[i]->getpos())
-					{
-						mover = 0;
-					}
-				}
-
-				aux.y++;
-				aux.x++;
-			} while (aux.x != fin.x || aux.y != fin.y);
-		}
-
-
-		//arriba izquierda
-		if (fin.x < p->getpos().x && fin.y > p->getpos().y)
-		{
-			do
-			{
-				for (int i = 0; i < 32; i++)
-				{
-					if (aux = lista[i]->getpos())
-					{
-						mover = 0;
-					}
-				}
-
-				aux.y++;
-				aux.x--;
-			} while (aux.x != fin.x || aux.y != fin.y);
-		}
-
-		//abajo derecha
-
-		if (fin.x > p->getpos().x && fin.y < p->getpos().y)
-		{
-			do
-			{
-				for (int i = 0; i < 32; i++)
-				{
-					if (aux = lista[i]->getpos())
-					{
-						mover = 0;
-					}
-				}
-
-				aux.y--;
-				aux.x++;
-			} while (aux.x != fin.x || aux.y != fin.y);
-		}
-
-		//abajo izquierda
-		if (fin.x < p->getpos().x && fin.y < p->getpos().y)
-		{
-			do
-			{
-				for (int i = 0; i < 32; i++)
-				{
-					if (aux = lista[i]->getpos())
-					{
-						mover = 0;
-					}
-				}
-
-				aux.y--;
-				aux.x--;
-			} while (aux.x != fin.x || aux.y != fin.y);
-		}
-
-
-		break;
-	}
-
-	case 6:{ mover = 1;
-		break;}
-			
-	
-	}
-	return mover;
-
-}
-
-void ListaPiezas::hayjaque()
+/*void ListaPiezas::hayjaque()
 {
 
 	Vector preynegro;
@@ -660,4 +286,4 @@ void ListaPiezas::jaquemate()
 
 
 
-}
+}*/
