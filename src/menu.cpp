780b294f36df5ea3aaf13a4c4b41482a6e5e0b78
evagombar <99722831+evagombar@users.noreply.github.com>
2022-07-10 16:26:56 +0200

    Update menu.cpp

--- src/menu.cpp
+++ src/menu.cpp
@@ -14,11 +14,6 @@ menu::~menu()
 {
 }
 
-void menu::teclaEspecial(unsigned char key)
-{
-	
-}
-
 void menu::tecla(unsigned char key)
 {
 	if (estado == INICIO)
@@ -27,7 +22,6 @@ void menu::tecla(unsigned char key)
 		{
 			mundo1.inicializa();
 			audio = true;
-			//nivel = 1;
 			tiempo = 0;
 			ETSIDI::stopMusica();
 			estado = JUEGO;
@@ -35,20 +29,6 @@ void menu::tecla(unsigned char key)
 		if (key == 's' || key == 'S')
 			exit(0);
 	}
-
-	else if (estado == FINJUEGO)
-	{
-		if (key == 'v' || key == 'V')
-		{
-			mundo1.~mundo();
-			audio = true;
-			ETSIDI::stopMusica();
-			estado = INICIO;
-		}
-		if (key == 's' || key == 'S')
-			exit(0);
-	}
-
 
 }
 
@@ -78,38 +58,18 @@ void menu::dibuja()
 			audio = false;
 			tiempo = 0;
 		}
-		if (tiempo > 1.5f && tiempo < 1.525f)
-			ETSIDI::playMusica("sonidos/menu.mp3", true);
+		
 	}
 
 	else if (estado == JUEGO)
-		mundo1.dibuja();
-	else if (estado == FINJUEGO)
-	{
-		gluLookAt(0, 7.5, 30,
-			0.0, 7.5, 0.0,
-			0.0, 1.0, 0.0);
-
-		ETSIDI::setTextColor(255, 255, 255);
-		ETSIDI::setFont("fuentes/Bitwise.ttf", 25);
-		ETSIDI::printxy("JAQUEMATE", -3, 9);
-		ETSIDI::setFont("fuentes/Bitwise.ttf", 12);
-		ETSIDI::printxy("PULSE LA TECLA -V- PARA VOLVER", -5, 6);
-		ETSIDI::printxy("PULSE LA TECLA -S- PARA SALIR", -5, 5);
-		if (audio) {
-			ETSIDI::play("sonidos/derrota.mp3");
-			audio = false;
-		}
-	}
+			mundo1.dibuja();
 }
 
 void menu::mueve()
 {
 	if (estado == JUEGO) {
+		ETSIDI::playMusica("sonidos/ambiente.mp3", true);
 		mundo1.mueve();
-		mundo1.dibuja();
-		
+		mundo1.dibuja();	
 	}
-	
 }
-
